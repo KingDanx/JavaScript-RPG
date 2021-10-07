c6a7927e825712c39a7388b4d32a954eeac029a1
Dan Prudhomme <dannyprud@yahoo.com>
2021-10-06 22:16:14 -0500

    added array of arrays to hold attack details. Added enemys with some stats. Working on attack function.

--- JSRPG.js
+++ JSRPG.js
@@ -9,9 +9,10 @@
 let player = {
     name: "Hercules",
     health: 100,
+    energy: 0, //max 100
     attack: {
-        basic: ["Bladestorm", "Overpower", "Falcon Punch"],
-        ultimate: "Execute"
+        basic: [["Bladestorm", 50, 50], ["Overpower", 10, 100], ["Falcon Punch", 100, 10]], //[name, damage, energy generation]
+        ultimate: ["Execute", 150, 0] //[name, damage, energy set point after use]
     }
 }
 
@@ -19,6 +20,7 @@ let enemy = [
     {
         name: "Nemean Lion",
         health: 200,
+        energy: 0,
         attack: {
             basic: ["Scratch", "Bite"],
             ultimate: "Hyper Fang"
@@ -27,14 +29,27 @@ let enemy = [
     {
         name: "Lernaean Hydra",
         health: 300,
+        energy: 0,
         attack: {
             basic: ["Tackle", "Stomp"],
             ultimate: "Exponential Bite"
         }
     },
-
 ];
+
+function playerAttackFunction(attackCommand){
+    for(let i = 0; i < player.attack.basic.length; i++){
+        if(player.attack.basic[i][0] === attackCommand){
+            player.energy += player.attack.basic[i][2];
+            alert(`${player.name} used ${player.attack.basic[i][0]} dealing ${player.attack.basic[i][1]} damage and generating ${player.attack.basic[i][2]} energy!`);
+        }
+}
+
 // As a user, I want the ability to select Hercules’ attack using a menu prompt.
+let playersAttack = prompt("What attack do you want to use?");
+function choosePlayerAttack(){
+   
+}
 // As a user, I want the foe’s attack to be chosen at random.
 // As a user, I want the results of each attack to be logged in the console.
 // As a developer, I want to use an Attack() function that will terminate when Hercules or an enemy’s health reaches zero.
